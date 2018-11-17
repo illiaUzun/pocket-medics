db9fb316e1ca257f6610f07f13d4f717a6255655
yunhaveto <yunhaveto@gmail.com>
2018-11-18 00:55:05 +0200

    Starting point

--- src/main/java/YELL/main/Controllers/ProfilesController.java
+++ src/main/java/YELL/main/Controllers/ProfilesController.java
@@ -1,14 +1,32 @@
 package YELL.main.Controllers;
 
-import YELL.main.Services.RegistrationService;
+import YELL.main.Services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import YELL.main.Entities.Account;
 
+import java.util.Optional;
 
 @RestController
-public class ProfilesController {
-    @Autowired
-    RegistrationService service;
+public class ProfileController {
 
+    /**
+     * Окно регистрации
+     * POST: отправка регистрационных данных в БД
+     */
+
+    @Autowired
+    ProfileService service;
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public void addUser(@RequestBody Account user) {
+        service.addUser(user);
+    }
+
+    @RequestMapping("/getusers")
+    public Account getUserByQueryStringId(@RequestParam(name = "id", required = true) long id) {
+        Optional<Account> student = service.getStudentById(id);
+        return student.orElse(null);
+    }
 
 }
--- src/main/java/YELL/main/Services/ProfilesService.java
+++ src/main/java/YELL/main/Services/ProfilesService.java
@@ -1,14 +1,28 @@
 package YELL.main.Services;
 
-import YELL.main.Repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import YELL.main.Entities.Account;
+import YELL.main.Repositories.AccountRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
-public class ProfilesService {
+public class ProfileService {
 
     @Autowired
-    ProfileRepository repository;
+    AccountRepository repository;
 
+    public List<Account> getAllUsers() {
+        return repository.findAll();
+    }
 
+    public void addUser(Account user) {
+        repository.saveAndFlush(user);
+    }
+
+    public Optional<Account> getStudentById(long id) {
+        return repository.findById(id);
+    }
 }
