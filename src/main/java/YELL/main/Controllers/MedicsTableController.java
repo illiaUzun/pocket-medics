f1e03d17dc267fc9be168f1e17a4cdb58f200eaa
yunhaveto <yunhaveto@gmail.com>
2018-11-18 04:26:22 +0200

    Добавил пример сортировки полученных

--- src/main/java/YELL/main/Controllers/MedicsTableController.java
+++ src/main/java/YELL/main/Controllers/MedicsTableController.java
@@ -1,16 +1,17 @@
 package YELL.main.Controllers;
 
-import YELL.main.Entities.Account;
 import YELL.main.Entities.Medic;
 import YELL.main.Services.MedicsTableService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.ProfileController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /**
- * Индекс медиков един с индексом аккаунтов!
+ * !!Индекс медиков един с индексом аккаунтов!!
+ *
+ *  1. Было бы круто выстроить запрос таким образом : localhost:8080/profiles/medics?category//это меняемый рофл//=ЛОР
+ *      в данном случае category выступает одновременно и PathVariable и RequestParam, как реализовать?
  *
  */
 
@@ -25,8 +26,13 @@ public class MedicsTableController {
         service.addUser(medic);
     }
 
+    @RequestMapping(value = "/profiles/medic", method = RequestMethod.GET)
+    public List<Medic> getMedicsByCategory(@RequestParam(name = "category", required = true) String category) {
+        return service.findMedicsByCategory(category);
+    }
+
     @RequestMapping(value = "/profiles/medics", method = RequestMethod.GET)
-    public List<Medic> getUserByCategory(@RequestParam(name = "category", required = true) String category) {
-        return service.findByCategory(category);
+    public List<Medic> getAllMedics() {
+        return service.getAllMedics();
     }
 }
