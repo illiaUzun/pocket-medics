300a6e5f314f9c671d863119a0b3ed335b97944c
yunhaveto <yunhaveto@gmail.com>
2019-01-11 20:50:05 +0200

    Added Favorites Repository

---
+++ src/main/java/YELL/main/Repositories/FavoritesRepository.java
@@ -0,0 +1,13 @@
+package YELL.main.Repositories;
+
+import YELL.main.Entities.Favorites;
+import YELL.main.Entities.Medic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FavoritesRepository extends JpaRepository<Favorites, Long> {
+
+    List<Favorites> getAllByAccountAccount_id (long id);
+
+}
