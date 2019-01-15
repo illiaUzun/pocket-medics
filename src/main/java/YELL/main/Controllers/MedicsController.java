50ea9c9776a04a93960bd949b0e52017314d8a6d
Илья Узун <uzun.illia.main@gmail.com>
2019-01-15 22:18:10 +0200

    Comments add/delete

--- src/main/java/YELL/main/Controllers/MedicsController.java
+++ src/main/java/YELL/main/Controllers/MedicsController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-import sun.jvm.hotspot.code.CompiledMethod;
 
 import java.util.List;
 import java.util.Optional;
