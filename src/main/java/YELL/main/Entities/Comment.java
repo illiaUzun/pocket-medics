43b4da20f8953069155260dccbf425e4e18db645
yunhaveto <yunhaveto@gmail.com>
2019-01-15 23:32:13 +0200

    mapped comment-acc 1-1

--- src/main/java/YELL/main/Entities/Comment.java
+++ src/main/java/YELL/main/Entities/Comment.java
@@ -10,26 +10,38 @@ public class Comment {
     @Column(name = "comment_id")
     @GeneratedValue
     private long id;
-    @Column(name = "accountId")
-    public long accountId;
+    //@Column(name = "accountId")
+    //public long accountId;
     @Column(name = "comment")
     private String comment;
+
+    @OneToOne(mappedBy = "comment", cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY, optional = false)
+    private Account account;
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 
     public Comment() {
     }
 
     public Comment(long accountId, String comment) {
-        this.accountId = accountId;
+        //this.accountId = accountId;
         this.comment = comment;
     }
 
-    public long getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(long accountId) {
-        this.accountId = accountId;
-    }
+//    public long getAccountId() {
+//        return accountId;
+//    }
+//
+//    public void setAccountId(long accountId) {
+//        this.accountId = accountId;
+//    }
 
     public String getComment() {
         return comment;
