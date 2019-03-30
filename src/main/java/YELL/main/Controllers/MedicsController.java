a47d9dd4c2eef49fad3bf530ca63fa30db56d1df
yunhaveto <yunhaveto@gmail.com>
2019-03-30 23:35:04 +0200

    forgot to clean up gson library

--- src/main/java/YELL/main/Controllers/MedicsController.java
+++ src/main/java/YELL/main/Controllers/MedicsController.java
@@ -5,7 +5,6 @@ import YELL.main.Entities.Medic;
 import YELL.main.Services.MedicService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
